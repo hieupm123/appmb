6efccd18fe5195d0707f1226d0973ec31206a9d4
future <future@future.com>
2022-09-10 17:23:41 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { BackHandler } from 'react-native';
+import { Linking} from 'react-native'
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
 
-export default function App() {
+
+function click() {
+  const url = 'https://alonewapppm.herokuapp.com/';
+  Linking.openURL(url);
+  alert("Nếu trang chủ sập liên hệ Lê Hoàng")
+  }
+
+
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+    <Text style={styles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <AppButton onPress={click} title="Hey there!" size="sm" backgroundColor="#007bff" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 16
   },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
 });
+
+export default App;
